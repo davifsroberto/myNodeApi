7fbca3c8447e11a55fefed530fc677737153318c
Davi Roberto <daviroberto11@hotmail.com>
2023-12-05 16:50:54 -0300

    test: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ src/utils/email-validator-adapter.spec.ts
@@ -0,0 +1,17 @@
+import { EmailValidatorAdapter } from './email-validator-adapter';
+
+describe('EmailValidator Adapter', () => {
+  test('Should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter();
+    const isValid = sut.isValid('invalid_email@mail.com');
+
+    expect(isValid).toBe(false);
+  });
+  
+  test('Should return false if validator returns false', () => {
+    const sut = new EmailValidatorAdapter();
+    const isValid = sut.isValid('invalid_email@mail.com');
+
+    expect(isValid).toBe(false);
+  });
+});
