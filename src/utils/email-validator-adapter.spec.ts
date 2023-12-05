cd5a922de8e19aadec04ae71540fa241426517d5
Davi Roberto <daviroberto11@hotmail.com>
2023-12-05 17:07:28 -0300

    test: ensure EmailValidatorAdapter call validator with correct email

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -24,4 +24,12 @@ describe('EmailValidator Adapter', () => {
 
     expect(isValid).toBe(true);
   });
+
+  test('Should call validator with correct email', () => {
+    const sut = new EmailValidatorAdapter();
+    const isEmailSpy = jest.spyOn(validator, 'isEmail');
+
+    sut.isValid('valid_email@mail.com');
+    expect(isEmailSpy).toHaveBeenCalledWith('valid_email@mail.com');
+  });
 });
