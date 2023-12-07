ea643442c9a8523006ba8f421130999bb5e173ef
Davi Roberto <daviroberto11@hotmail.com>
2023-12-07 17:31:18 -0300

    feat: ensure AccountMongoRepository returns an account on success

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,4 +1,4 @@
-import { MongoClient } from 'mongodb';
+import { Collection, MongoClient } from 'mongodb';
 
 export const MongoHelper = {
   client: MongoClient,
@@ -9,7 +9,11 @@ export const MongoHelper = {
     this.client = await MongoClient.connect(String(process.env.MONGO_URL));
   },
 
-  async disconnect(): Promise<void> {
-    await this.client.disconnect();
+  async close(): Promise<void> {
+    await this.client.close();
+  },
+
+  getCollection(name: string): Collection {
+    return this.client.db().collection(name);
   },
 };
