df51960e077d27ea84fdb6f28698a7b5f0a6ae06
Davi Roberto <daviroberto11@hotmail.com>
2023-12-08 17:03:31 -0300

    chore: update jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ import type { Config } from 'jest';
 
 const config: Config = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**'],
   coverageDirectory: 'coverage',
   collectCoverage: true,
   clearMocks: true,
