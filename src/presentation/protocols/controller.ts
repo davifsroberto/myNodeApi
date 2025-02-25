d52c3836758a6fb702a99c872a584ee454e8f3da
Davi Roberto <davi.freire@hsystem.com.br>
2025-02-25 08:43:00 -0300

    feat: add Controller interface for handling HTTP requests

---
+++ src/presentation/protocols/controller.ts
@@ -0,0 +1,6 @@
+import { HttpRequest, HttpResponse } from './http';
+
+export interface Controller {
+  // eslint-disable-next-line no-unused-vars
+  handle(httpRequest: HttpRequest): HttpResponse;
+}
