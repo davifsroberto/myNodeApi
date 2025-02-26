82419d0d7f3981a0625288fd0bf70a910f90c2e8
Davi Roberto <davi.freire@hsystem.com.br>
2025-02-26 14:17:39 -0300

    feat: enhance SignUpController to validate password confirmation and integrate AddAccount use case

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -6,12 +6,15 @@ import {
   HttpRequest,
   HttpResponse,
 } from '../protocols';
+import { AddAccount } from '../../domain/usecases/add-account';
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
+  private readonly addAccount: AddAccount;
 
-  constructor(emailValidator: EmailValidator) {
+  constructor(emailValidator: EmailValidator, addAccount: AddAccount) {
     this.emailValidator = emailValidator;
+    this.addAccount = addAccount;
   }
 
   handle(httpRequest: HttpRequest): HttpResponse {
@@ -24,14 +27,21 @@ export class SignUpController implements Controller {
       ];
 
       const reqBody = httpRequest.body;
+      const { name, email, password, passwordConfirmation } = reqBody;
 
       for (const field of requiredFields) {
         if (!reqBody[field]) return badRequest(new MissingParamError(field));
       }
 
-      const isValid = this.emailValidator.isValid(reqBody.email);
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'));
+      }
 
-      if (!isValid) return badRequest(new InvalidParamError('email'));
+      const isEmailValid = this.emailValidator.isValid(email);
+
+      if (!isEmailValid) return badRequest(new InvalidParamError('email'));
+
+      this.addAccount.add({ name, email, password });
     } catch (error) {
       return serverError();
     }
