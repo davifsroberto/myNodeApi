9e3b8dd3bb280bbc1bfd1e5ad7ddedb4cbdff2ce
Davi Roberto <davi.freire@hsystem.com.br>
2025-02-26 13:38:39 -0300

    feat: enhance SignUpController to handle missing parameters and server errors

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,9 +1,11 @@
-import { MissingParamError } from '../erros/missing-param-error';
-import { HttpRequest, HttpResponse } from '../protocols/http';
-import { badRequest } from '../helpers/http-helper';
-import { Controller } from '../protocols/controller';
-import { EmailValidator } from '../protocols/email-validator';
-import { InvalidParamError } from '../erros/invalid-param-error';
+import { badRequest, serverError } from '../helpers/http-helper';
+import { InvalidParamError, MissingParamError } from '../erros';
+import {
+  Controller,
+  EmailValidator,
+  HttpRequest,
+  HttpResponse,
+} from '../protocols';
 
 export class SignUpController implements Controller {
   private readonly emailValidator: EmailValidator;
@@ -13,22 +15,26 @@ export class SignUpController implements Controller {
   }
 
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = [
-      'name',
-      'email',
-      'password',
-      'passwordConfirmation',
-    ];
+    try {
+      const requiredFields = [
+        'name',
+        'email',
+        'password',
+        'passwordConfirmation',
+      ];
 
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+      const reqBody = httpRequest.body;
+
+      for (const field of requiredFields) {
+        if (!reqBody[field]) return badRequest(new MissingParamError(field));
       }
+
+      const isValid = this.emailValidator.isValid(reqBody.email);
+
+      if (!isValid) return badRequest(new InvalidParamError('email'));
+    } catch (error) {
+      return serverError();
     }
-
-    const isValid = this.emailValidator.isValid(httpRequest.body.email);
-
-    if (!isValid) return badRequest(new InvalidParamError('email'));
 
     return {
       statusCode: 200,
