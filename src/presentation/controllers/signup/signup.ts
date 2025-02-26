c0189a46b9d4ee59175bd038e15f1af01ff829a5
Davi Roberto <davi.freire@hsystem.com.br>
2025-02-26 14:34:42 -0300

    feat: update SignUpController to return account details upon successful account creation

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -41,16 +41,14 @@ export class SignUpController implements Controller {
 
       if (!isEmailValid) return badRequest(new InvalidParamError('email'));
 
-      this.addAccount.add({ name, email, password });
+      const account = this.addAccount.add({ name, email, password });
+
+      return {
+        statusCode: 200,
+        body: account,
+      };
     } catch (error) {
       return serverError();
     }
-
-    return {
-      statusCode: 200,
-      body: {
-        message: 'Success',
-      },
-    };
   }
 }
