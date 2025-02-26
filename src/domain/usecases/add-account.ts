5b517237151e676657f5bdeb02af28ced830a76f
Davi Roberto <davi.freire@hsystem.com.br>
2025-02-26 14:17:16 -0300

    feat: add AddAccountModel interface and AddAccount use case for account creation

---
+++ src/domain/usecases/add-account.ts
@@ -0,0 +1,12 @@
+import { AccountModel } from '../models/account';
+
+export interface AddAccountModel {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface AddAccount {
+  // eslint-disable-next-line no-unused-vars
+  add(account: AddAccountModel): AccountModel;
+}
