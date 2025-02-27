2481f7bea431653f4811f753ab73c8b19bfd39fd
Davi Roberto <davi.freire@hsystem.com.br>
2025-02-27 08:22:21 -0300

    feat: add EmailValidatorAdapter for email validation using validator library

---
+++ src/utils/email-validator-adapter.ts
@@ -0,0 +1,9 @@
+import validator from 'validator';
+
+import { EmailValidator } from '../presentation/protocols/email-validator';
+
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid(email: string): boolean {
+    return validator.isEmail(email);
+  }
+}
