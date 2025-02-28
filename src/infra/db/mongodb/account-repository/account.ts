c981c0883014ab3b7940c0938cf9ee620b47ee2f
Davi Roberto <davi.freire@hsystem.com.br>
2025-02-28 09:18:35 -0300

    feat: implement MongoDB account repository and helper functions

---
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -0,0 +1,18 @@
+import { AddAccountRepository } from '../../../../data/protocols/add-account-repository';
+import { AccountModel } from '../../../../domain/models/account';
+import { AddAccountModel } from '../../../../domain/usecases/add-account';
+import { AccountModelInserted, MongoHelper } from '../helpers/mongo-helper';
+
+export class AccountMongoRepository implements AddAccountRepository {
+  async add(accountData: AddAccountModel): Promise<AccountModel> {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+    const result = await accountCollection.insertOne(accountData);
+    const insertedUserCursor = await accountCollection.find({
+      _id: result.insertedId,
+    });
+    const insertedUser = await insertedUserCursor.toArray();
+    const account = insertedUser[0] as AccountModelInserted;
+
+    return MongoHelper.map<AccountModelInserted, AccountModel>(account);
+  }
+}
