d6c158fef6036fa746e82509bb1d8d3316883482
Davi Roberto <davi.freire@hsystem.com.br>
2025-03-01 15:01:03 -0300

    feat: connect to MongoDB and update server startup logic

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,5 +1,11 @@
 import app from './config/app';
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper';
+import env from './config/env';
 
-app.listen(5050, () => {
-  console.log('Server is running at http://localhost:5050');
+MongoHelper.connect(env.mongoUrl).then(async () => {
+  (await import('./config/app')).default;
+
+  app.listen(env.port, () => {
+    console.log(`Server is running at http://localhost:${env.port}`);
+  });
 });
