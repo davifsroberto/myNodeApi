476c280a50d0b6590689f05b5ad33c6e3d267964
Davi Roberto <davi.freire@hsystem.com.br>
2025-03-01 15:01:39 -0300

    test: enhance signup route tests with MongoDB connection setup and teardown

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,22 @@
 import request from 'supertest';
 import app from '../config/app';
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper';
 
 describe('SignUp Routers', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(String(process.env.MONGO_URL));
+  });
+
+  afterAll(async () => {
+    await MongoHelper.disconnect();
+  });
+
+  beforeEach(async () => {
+    const accountCollection = await MongoHelper.getCollection('accounts');
+
+    await accountCollection.deleteMany({});
+  });
+
   it('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
